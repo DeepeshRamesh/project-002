d458e828100717381fa615d6d3aa2bb8e3965a27
DeepeshRamesh <ddaisuke69@gmail.com>
2026-03-02 17:30:42 +0530

    updated the password logic and used hashing

--- project-002-backend/server.js
+++ project-002-backend/server.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
 
@@ -6,9 +7,26 @@ app.use(cors());
 app.use(express.json());
 
 let users = [];
+const tokens = [];
+
+function authMiddleware(req,res,next){
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader){
+        return res.status(401).json({ message:"No token provided"});
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!tokens.includes(token)){
+        return res.status(403).json({message:"Invalid token"});
+    }
+
+    next();
+}
 
 //Signup endpoint
-app.post("/signup",(req,res) => {
+app.post("/signup", async (req,res) => {
     const { email, password } = req.body;
 
     const existingUser = users.find((user) => user.email === email );
@@ -19,18 +37,18 @@ app.post("/signup",(req,res) => {
         });
     }
 
-    users.push({ email , password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({ email , hashedPassword });
 
     res.json({ message: "Signup successful" });
 });
 
 //Login endpoint
-app.post("/login", (req,res) => {
+app.post("/login", async (req,res) => {
     const { email , password } = req.body;
 
     const user = users.find(
-        (u) => u.email === email && u.password === password
-    );
+        (u) => u.email === email);
 
     if (!user){
         return res.status(401).json({
@@ -38,12 +56,32 @@ app.post("/login", (req,res) => {
         });
     }
 
+    const passwordMatch = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if (!passwordMatch){
+        return res.status(401).json({
+            message: "Invalid credentials",
+        });
+    }
+
     const fakeToken = "token-" + Date.now();
+    tokens.push(fakeToken);
 
     res.json({
         message: "Login successful",
         token: fakeToken,
         email: user.email,
+    });
+});
+
+//profile
+app.get("/profile", authMiddleware,(req,res)=>{
+    res.json({
+        message: "Protected data access granted",
+        data: "Sensitive user information",
     });
 });
 
--- project-002/src/pages/Dashboard.jsx
+++ project-002/src/pages/Dashboard.jsx
@@ -1,5 +1,39 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 function Dashboard(){
-    return <h1>Dashboard (Protected)</h1>;
+    const { token } = useContext(AuthContext);
+    const [ data, setData ] = useState("");
+
+    useEffect(() => {
+        const fetchProtectedData = async () => {
+            try{
+                const res = await fetch("https://localhost:5000/profile",{
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const result = await res.json();
+
+                if (res.ok) {
+                    setData(result.data);
+                }else{
+                    setData(result.message);
+                }
+            }catch(error) {
+                setData("Server error");
+            }
+        };
+
+        fetchProtectedData();
+    }, [token]);
+
+    return (
+        <div style={{ padding: "40px" }}>
+            <h1>Dashboard</h1>
+            <p>{data}</p>
+        </div>
+    );
 }
 
 export default Dashboard;
